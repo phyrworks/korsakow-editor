42de35029831292c4db42ac55a431ac0d1e74766
David Reisch <d@putgeminmouth.com>
2014-06-25 21:43:26 -0400

    -Fixed example scrubber colors -Made CreateDummyProject better

--- unit-tests/src/test/org/korsakow/CreateDummyProject.java
+++ unit-tests/src/test/org/korsakow/CreateDummyProject.java
@@ -12,13 +12,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+import java.util.prefs.Preferences;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
@@ -46,8 +51,8 @@ import org.korsakow.ide.util.FileUtil;
 public class CreateDummyProject
 {
 	public static final Random rand = new Random();
-	public static String getRand(String... s) {
-		return s[rand.nextInt(s.length)];
+	public static String getRand(List<String> s) {
+		return s.get(rand.nextInt(s.size()));
 	}
 	public static void main(String[] args) throws Exception {
 		Main.setupLogging();
@@ -62,15 +67,30 @@ public class CreateDummyProject
 			}
 		});
 	}
+	private static String getPref(String p, String def) {
+		return Preferences.userNodeForPackage(CreateDummyProject.class).get(p, def);
+	}
+	private static void setPref(String p, String value) {
+		Preferences.userNodeForPackage(CreateDummyProject.class).put(p, value);
+	}
 	public static void doFrame() throws Exception {
-		JFrame frame = new JFrame();
+		final JFrame frame = new JFrame();
 		frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 		
 		JPanel p;
 		
 		p = new JPanel(new FlowLayout());
+		final JButton resourcesDirButton = new JButton("...");
+		final JTextField resourcesDirField = new JTextField(getPref("resourcesDir", String.format("%s/main/src/resources", new File(".").getAbsolutePath())));
+		resourcesDirField.setPreferredSize(new Dimension(440, 25));
+		p.add(new JLabel("Dummy Media Dir"));
+		p.add(resourcesDirField);
+		p.add(resourcesDirButton);
+		frame.add(p);
+		
+		p = new JPanel(new FlowLayout());
 		final JButton workDirButton = new JButton("...");
-		final JTextField workDirField = new JTextField(String.format("%s/tmp/kdummy", System.getProperty("user.home")));
+		final JTextField workDirField = new JTextField(getPref("workDir", String.format("%s/korsakow/tmp/dummy", System.getProperty("user.home"))));
 		workDirField.setPreferredSize(new Dimension(440, 25));
 		p.add(new JLabel("Work Dir"));
 		p.add(workDirField);
@@ -79,16 +99,16 @@ public class CreateDummyProject
 		
 		p = new JPanel(new FlowLayout());
 		final JButton inFileButton = new JButton("...");
-		final JTextField inFileField = new JTextField();
+		final JTextField inFileField = new JTextField(getPref("projectFile", ""));
 		inFileField.setPreferredSize(new Dimension(440, 25));
-		p.add(new JLabel("Source File"));
+		p.add(new JLabel("Source (KRW) File"));
 		p.add(inFileField);
 		p.add(inFileButton);
 		frame.add(p);
 		
 		p = new JPanel(new FlowLayout());
 		final JButton outFileButton = new JButton("...");
-		final JTextField outFileField = new JTextField("/Users/d/Work/Korsakow/export/default/");
+		final JTextField outFileField = new JTextField(getPref("ourDir", String.format("%s/korsakow/tmp/export", System.getProperty("user.home"))));
 		outFileField.setPreferredSize(new Dimension(440, 25));
 		p.add(new JLabel("Out Dir"));
 		p.add(outFileField);
@@ -97,6 +117,18 @@ public class CreateDummyProject
 		
 		JButton okButton = new JButton("OK");
 		frame.add(okButton);
+		
+		resourcesDirButton.addActionListener(new ActionListener() {
+			@Override public void actionPerformed(ActionEvent e) {
+				JFileChooser chooser = new JFileChooser(".");
+				chooser.setFileSelectionMode( JFileChooser.DIRECTORIES_ONLY );
+				chooser.showOpenDialog((Component)e.getSource());
+				File file = chooser.getSelectedFile();
+				if ( file == null )
+					return;
+				resourcesDirField.setText(file.getPath());
+			};
+		});
 		
 		inFileButton.addActionListener(new ActionListener() {
 			@Override public void actionPerformed(ActionEvent e) {
@@ -138,8 +170,14 @@ public class CreateDummyProject
 				String basePath = workDirField.getText();
 				String inPath = inFileField.getText();
 				String outPath = outFileField.getText();
+				String resourcePath = resourcesDirField.getText();
 				try {
-					create( basePath, inPath, new File(outPath, new File(inPath).getName()).getPath() );
+					setPref("workDir", basePath);
+					setPref("outDir", outPath);
+					setPref("resourcesDir", resourcePath);
+					setPref("projectFile", inPath);
+					create( basePath, inPath, new File(outPath, new File(inPath).getName()).getPath(), resourcePath );
+					JOptionPane.showMessageDialog(frame, "Done");
 				} catch (Exception e1) {
 					throw new RuntimeException(e1);
 				}
@@ -151,7 +189,10 @@ public class CreateDummyProject
 		frame.setSize(640, 480);
 		frame.setVisible(true);
 	}
-	public static void create(String basePath, String inPath, String outPath) throws CommandException, FileNotFoundException, IOException, SQLException, KeyNotFoundException, CreationException, MapperException, TransformerException {
+	public static void create(String basePath, String inPath, String outPath, String resourcePath) throws CommandException, FileNotFoundException, IOException, SQLException, KeyNotFoundException, CreationException, MapperException, TransformerException {
+		
+		System.out.println("Finding media files in " + resourcePath);
+		findDummyFiles(new File(resourcePath));
 		
 		Request request = new Request();
 		request.set("filename", inPath);
@@ -218,29 +259,35 @@ public class CreateDummyProject
 		fileOutputStream.flush();
 		fileOutputStream.close();
 	}
+	private static List<String> dummyImages = new ArrayList<String>();
+	private static List<String> dummyVideos = new ArrayList<String>();
+	private static List<String> dummySounds = new ArrayList<String>();
+	
+	private static void findDummyFiles(File dir) {
+		File[] files = dir.listFiles();
+		if (files == null)
+			return;
+		for (File file : files) {
+			findDummyFiles(file);
+			String ext = FileUtil.getFileExtension(file.getName()).toLowerCase();
+			if (Arrays.asList("jpg", "jpeg", "gif", "png").contains(ext))
+				dummyImages.add(file.getPath());
+			if (Arrays.asList("flv", "mov", "mp4", "m4v", "avi").contains(ext))
+				dummyVideos.add(file.getPath());
+			if (Arrays.asList("mp3", "aac", "wav").contains(ext))
+				dummySounds.add(file.getPath());
+		}
+	}
 	private static File getDummyImageFile()
 	{
-		final File blankFile = new File(getRand(
-				"/Users/d/Work/Korsakow/media/images/placeholder.jpg"
-				));
-		return blankFile;
+		return new File(getRand(dummyImages));
 	}
 	private static File getDummyVideoFile()
 	{
-		final File blankFile = new File(getRand(
-				"/Users/d/Work/Korsakow/media/videos/mokay.mov"
-//				"/Users/d/Work/Korsakow/media/videos/IronMan4.mov",
-//				"/Users/d/Work/Korsakow/media/videos/man01.mov",
-//				"/Users/d/Work/Korsakow/media/videos/Hatschifanta.flv",
-//				"/Users/d/Work/Korsakow/media/videos/aesthetics1.mov"
-				));
-		return blankFile;
+		return new File(getRand(dummyVideos));
 	}
 	private static File getDummySoundFile()
 	{
-		final File blankFile = new File(getRand(
-				"/Users/d/Work/Korsakow/media/sounds/clips/train.wav"
-				));
-		return blankFile;
+		return new File(getRand(dummySounds));
 	}
 }
